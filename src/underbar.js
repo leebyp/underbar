c522ebad922c8fa1a827301181cb9ac9a4f438f6
Benjamin Lee <leebyp@gmail.com>
2014-02-19 19:47:04 +0800

    Corrected all OBJECTS in underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -224,11 +224,23 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i=1; i<arguments.length; i++){
+      for (var x in arguments[i]){
+        obj[x] = arguments[i][x];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i=1; i<arguments.length; i++){
+      for (var x in arguments[i]){
+        obj[x] = (obj[x] !== undefined ? obj[x] : arguments[i][x]);
+      }
+    }
+    return obj;
   };
 
 
